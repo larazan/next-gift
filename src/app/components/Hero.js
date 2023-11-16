c62f36f096a617dce3ac152ef588dbc6e102603b
zamroni <forheron@gmail.com>
2023-11-16 18:18:07 +0700

    fix

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -8,15 +8,15 @@ import product from "@/assets/iwatch.png";
     <>
       <div className="bg-[#f2f6f6] flex relative z-20 items-center overflow-hidden">
         <div className="container2 mx-auto px-6 flex items-center relative py-12">
-            <div className="sm:w-2/3 lg:w-2/5 px-6 flex flex-col relative z-20 ">
+            <div className="sm:w-2/3 lg:w-2/5 px-6 flex flex-col relative z-20 text">
                 
-                <h1 className="font-bebas-neue uppercase text-3xl sm:text-5xl font-bold flex flex-col leading-none dark:text-white text-[#b02768]">
+                <h1 className="font-bebas-neue uppercase text-3xl sm:text-5xl font-bold flex flex-col leading-none  text-[#b02768]">
                 Life&apos;s a party.
                 </h1>
-                <h3 className='text-2xl font-semibold flex flex-col py-4 leading-tight tracking-tight dark:text-white text-[#1d4943]'>
+                <h3 className='text-2xl font-semibold flex flex-col py-4 leading-tight tracking-tight  text-[#1d4943]'>
                 Gift responsibly.
                 </h3>
-                <p className="text-sm sm:text-base text-gray-700 dark:text-white tracking-tight">
+                <p className="text-sm sm:text-base text-gray-700  tracking-tight">
                     Dimension of reality that makes change possible and understandable. An indefinite and homogeneous environment in which natural events and human existence take place.
                 </p>
                 <div className="flex mt-8">
