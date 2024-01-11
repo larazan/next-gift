66eec027f87413ee13c3c2e44e0533c13edd1139
zamroni <forheron@gmail.com>
2024-01-11 18:45:59 +0700

    fix button cart

--- src/app/components/Ingredient.js
+++ src/app/components/Ingredient.js
@@ -12,38 +12,48 @@ const Ingredient = () => {
           <Tabs>
             <Tab label="Overview">
               <div className="flex flex-col md:flex-row w-full py-4">
-              <div className="w-1/2 space-y-4 leading-snug [&amp;_h1:not(:last-child)]:mb-4 [&amp;_h1:not(:last-child)]:md:mb-6 [&amp;_h1]:border-b [&amp;_h1]:border-black [&amp;_h1]:text-base [&amp;_h1]:md:border-none [&amp;_h1]:md:text-3xl [&amp;_h1]:md:leading-[0.8] [&amp;_h2:not(:last-child)]:mb-4 [&amp;_h2:not(:last-child)]:md:mb-6 [&amp;_h2]:text-base [&amp;_h2]:md:text-3xl [&amp;_h3]:font-sans [&amp;_h3]:text-base [&amp;_h3]:font-bold [&amp;_h4]:font-sans [&amp;_h4]:text-base [&amp;_h4]:font-bold [&amp;_h5]:font-sans [&amp;_h5]:text-base [&amp;_h5]:font-bold [&amp;_h6]:font-sans [&amp;_h6]:text-base [&amp;_h6]:font-bold [&amp;_ol[type='a']]:list-[lower-alpha] [&amp;_ol]:ml-[24px] [&amp;_ol]:list-outside [&amp;_ol]:list-decimal [&amp;_ol]:leading-tight [&amp;_ol_li::marker]:ml-[-24px] [&amp;_p]:leading-tight [&amp;_table]:table-auto [&amp;_table_td]:border [&amp;_table_td]:border-black [&amp;_table_td]:px-4 [&amp;_table_td]:py-2 [&amp;_table_td]:text-sm [&amp;_table_td]:md:text-base [&amp;_table_th]:border [&amp;_table_th]:border-black [&amp;_table_th]:px-4 [&amp;_table_th]:py-2 [&amp;_table_th]:text-sm [&amp;_table_th]:md:text-base [&amp;_ul]:ml-[24px] [&amp;_ul]:list-outside [&amp;_ul]:list-disc [&amp;_ul]:leading-tight [&amp;_ul_li::marker]:ml-[-24px] my-4">
-          <p className=" text-gray-900">
-            At PLNTS.com, we love to try and discover new things. Especially if
-            it has to do with new plants! At first, we thought this tryst was a
-            failure, but after a bit more patience and growth, we saw the beauty
-            of the plant. Wow, what a beauty right?!
-          </p>
-          <p className=" text-gray-900">
-            She is a completely new Monstera species. Grown by PLNTS.com and
-            PLNTS.com is also the only one allowed to grow the plant! So with
-            this plant, you ABSOLUTELY get a unique and special plant in your
-            home. This plant owes her name to our community, they came up with
-            the name Frozen Freckles. If you look at her, you will of course
-            understand why.
-          </p>
-          <p className=" text-gray-900">
-            <strong>
-              Please note: PLNTS.com holds the breeders&lsquo; rights to this plant.
-              This means that it is not allowed to trade the plant or cuttings
-              from the plant yourself.{" "}
-            </strong>
-          </p>
-        </div> 
-               
+                <div className="w-1/2 space-y-4 leading-snug [&amp;_h1:not(:last-child)]:mb-4 [&amp;_h1:not(:last-child)]:md:mb-6 [&amp;_h1]:border-b [&amp;_h1]:border-black [&amp;_h1]:text-base [&amp;_h1]:md:border-none [&amp;_h1]:md:text-3xl [&amp;_h1]:md:leading-[0.8] [&amp;_h2:not(:last-child)]:mb-4 [&amp;_h2:not(:last-child)]:md:mb-6 [&amp;_h2]:text-base [&amp;_h2]:md:text-3xl [&amp;_h3]:font-sans [&amp;_h3]:text-base [&amp;_h3]:font-bold [&amp;_h4]:font-sans [&amp;_h4]:text-base [&amp;_h4]:font-bold [&amp;_h5]:font-sans [&amp;_h5]:text-base [&amp;_h5]:font-bold [&amp;_h6]:font-sans [&amp;_h6]:text-base [&amp;_h6]:font-bold [&amp;_ol[type='a']]:list-[lower-alpha] [&amp;_ol]:ml-[24px] [&amp;_ol]:list-outside [&amp;_ol]:list-decimal [&amp;_ol]:leading-tight [&amp;_ol_li::marker]:ml-[-24px] [&amp;_p]:leading-tight [&amp;_table]:table-auto [&amp;_table_td]:border [&amp;_table_td]:border-black [&amp;_table_td]:px-4 [&amp;_table_td]:py-2 [&amp;_table_td]:text-sm [&amp;_table_td]:md:text-base [&amp;_table_th]:border [&amp;_table_th]:border-black [&amp;_table_th]:px-4 [&amp;_table_th]:py-2 [&amp;_table_th]:text-sm [&amp;_table_th]:md:text-base [&amp;_ul]:ml-[24px] [&amp;_ul]:list-outside [&amp;_ul]:list-disc [&amp;_ul]:leading-tight [&amp;_ul_li::marker]:ml-[-24px] my-4">
+                  <p className="text-gray-900 font-bold">Suitable for</p>
+                  <p className=" text-gray-900">All Skin Types</p>
+                  <p className="text-gray-900 font-bold">
+                    Why we stand behind it
+                  </p>
+                  <p className=" text-gray-900">
+                    The first overnight moisturizer clinically proven to reduce
+                    the effects of stress on skin during its nightly repair
+                    phase. This ultra-hydrating, plumping cream with
+                    algae-derived bio retinol, vegan collagen plus five vital
+                    hydrators, firms and smooths the look of lines and wrinkles.
+                    Wake up to rested, refreshed, radiant skin.
+                  </p>
+                  <p className="text-gray-900 font-bold">Key ingredients</p>
+                  <p className=" text-gray-900">
+                    Bio Retinol: Extract derived from blue-green microalgae,
+                    visibly improves lines and wrinkles without irritation.
+                    Vegan Collagen: Plant-derived collagen alternative, rich in
+                    amino acids. King of Bitters: A super-plant that improves
+                    signs of fatigue on tired skin, including wrinkles. Pink
+                    Rock-Rose Extract: Improves skin dullness and dehydration
+                    for healthier, more youthful-looking skin Wild Lavender:
+                    Known for its calming and relaxing effects, this extract
+                    increases hydration and improves visible signs of skin
+                    fatigue. 5 Vital Hydrators: Revive dry skin leaving it soft,
+                    smooth and deeply moisturized.
+                  </p>
+                </div>
               </div>
             </Tab>
             <Tab label="How To Use">
               <div className="py-4">
                 <div className="w-1/2">
-                    <h3 className="text-lg md:text-2xl capitalize font-semibold">usage</h3>
+                  <h3 className="text-lg md:text-2xl capitalize font-semibold">
+                    usage
+                  </h3>
                   <p className="py-2 text-gray-700 text-md md:text-md">
-                  For best anti-aging results, layer over Hyaluronic Sea Serum or Anti-Aging Sea Serum. On nights using Seaglow Overnight Serum, layer over for the perfect exfoliation + hydration night time routine.
+                    For best anti-aging results, layer over Hyaluronic Sea Serum
+                    or Anti-Aging Sea Serum. On nights using Seaglow Overnight
+                    Serum, layer over for the perfect exfoliation + hydration
+                    night time routine.
                   </p>
                 </div>
               </div>
@@ -57,20 +67,27 @@ const Ingredient = () => {
                   <p>This diet contains 4,203 kcal ME/kg; 2.5 kcal ME/treat.</p>
                 </div>
                 <div className="w-1/2">
-                  <h3 className="text-md md:text-lg font-bold">
-                    Fragrance
-                  </h3>
+                  <h3 className="text-md md:text-lg font-bold">Fragrance</h3>
                   <p>Calming & relaxing lavender</p>
                 </div>
               </div>
             </Tab>
             <Tab label="About the brand">
               <div className="flex py-4 w-full">
-              <div className="w-1/2">
-                    <h3 className="text-lg md:text-2xl uppercase font-semibold">OSEA</h3>
+                <div className="w-1/2">
+                  <h3 className="text-lg md:text-2xl uppercase font-semibold">
+                    OSEA
+                  </h3>
                   <p className="py-2 text-gray-700 text-md md:text-md">
-                  Inspired by her grandmother’s lifelong love of the sea, Jenefer Palmer set out to share her passion through a plant-based skin care line infused with elements of the ocean. Distraught by the harmful chemicals she found in other products, Palmer resolved to craft her formulas with safer, nourishing ingredients.
-While OSEA has now expanded into a world-renowned skin care line, their commitment has stayed the same—producing effective, wholesome products as pure as the ocean itself.
+                    Inspired by her grandmother’s lifelong love of the sea,
+                    Jenefer Palmer set out to share her passion through a
+                    plant-based skin care line infused with elements of the
+                    ocean. Distraught by the harmful chemicals she found in
+                    other products, Palmer resolved to craft her formulas with
+                    safer, nourishing ingredients. While OSEA has now expanded
+                    into a world-renowned skin care line, their commitment has
+                    stayed the same—producing effective, wholesome products as
+                    pure as the ocean itself.
                   </p>
                 </div>
               </div>
